a8e59cc907205686062f3af77a06cee7d2b3beb9
samoolean <samuel.choisi1@gmail.com>
2025-03-16 13:38:11 +0100

    patch menu

--- js/header.js
+++ js/header.js
@@ -1,3 +1,5 @@
+/*
+
 let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
@@ -10,3 +12,38 @@ window.onscroll = () => {
     navbar.classList.remove('active');
     menu.classList.remove('bx-x');
 }
+
+custom*/
+
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId);
+    const nav = document.getElementById(navId);
+
+    if(toggle && nav){
+
+        console.log("yes");
+        
+        toggle.addEventListener('click', () => { nav.classList.toggle('show'); }) 
+    
+    } else{
+
+        console.log("NO");
+
+    }
+}
+
+showMenu('nav-toggle','nav-menu');
+
+// Toggling Mobile Menu when a navlink is clicked
+
+const navLink = document.querySelectorAll('.nav-link');
+
+function linkAction() {
+    navLink.forEach(n => n.classList.remove('active'))
+    this.classList.add('active');
+
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.remove('show');
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction));
